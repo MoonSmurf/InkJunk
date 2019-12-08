76579a04a44b9203bd25e567969953a49e5d1062
MoonSmurf <57339847+MoonSmurf@users.noreply.github.com>
2019-12-08 12:10:27 -0600

    Add files via upload

---
+++ Java-j/test.js
@@ -0,0 +1,24 @@
+
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("Thanks for signing up!");
+      return false;
+   }
+   
+   document.getElementById("delivery").onclick = turnOnDelivery;
+   document.getElementById("pickup").onclick=turnOnPickup;
+}
+
+function turnOnDelivery() {
+   document.getElementById("addressBox").disabled=false;
+   document.getElementById("delBox").disabled=false;
+   document.getElementById("pickupBox").disabled=true;
+}
+
+function turnOnPickup() {
+   document.getElementById("addressBox").disabled=true;
+   document.getElementById("delBox").disabled=true;
+   document.getElementById("pickupBox").disabled=false;
+}
