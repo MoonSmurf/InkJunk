ebdea476f66fc3cc65c89263156b782a909fa5d0
MoonSmurf <57339847+MoonSmurf@users.noreply.github.com>
2019-12-08 12:32:31 -0600

    Add files via upload

--- Java-j/test.js
+++ Java-j/test.js
@@ -1,5 +1,5 @@
 
-/*window.onload = checkit;
+window.onload = checkit;
 
 function checkit() {
    document.forms[0].onsubmit = function() {
@@ -11,7 +11,7 @@ function checkit() {
    document.getElementById("pickup").onclick=turnOnPickup;
 }
 
-function turnOnDelivery() {
+/*function turnOnDelivery() {
    document.getElementById("addressBox").disabled=false;
    document.getElementById("delBox").disabled=false;
    document.getElementById("pickupBox").disabled=true;
@@ -22,11 +22,4 @@ function turnOnPickup() {
    document.getElementById("delBox").disabled=true;
    document.getElementById("pickupBox").disabled=false;
 }*/
-
-function  submitInfo() {
-	var userName = document.getElementById("nameB");
-	var email = document.getElementById("eMail");
-	(userName.value && email.value) ? alert("Thanks for signing up!") : alert("Please fill in missing info");
-
-}
 
